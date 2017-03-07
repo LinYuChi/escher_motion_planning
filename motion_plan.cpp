2a9b79f16b8a089ba16c1f7d591871eaa0118c3a
Deen Adzemovic <bedeen@umich.edu>
2017-03-07 17:11:06 -0500

    query progress

--- motion_plan.cpp
+++ motion_plan.cpp
@@ -1,3 +1,7 @@
 #include "motion_plan.h"
 
+using std::vector;
 
+vector<Motion_plan> Motion_plan_library::query(const vector<Contact_region> & contact_regions) const {
+	
+}
--- motion_plan.h
+++ motion_plan.h
@@ -6,6 +6,7 @@
 
 #include <vector>
 #include <string>
+#include <map>
 
 struct Contact_region {
 	OpenRAVE::Vector position; // region center
@@ -28,11 +29,28 @@ struct Contact {
 
 struct Motion_plan {
 	std::vector<std::vector<std::pair<Manip, int> > > waypoint_contact_manips;
-	std::vector<RPY_tf> contact_sequence;
+	std::vector<Contact> contact_sequence;
 	std::vector<Node> contact_plan; // each node differs from previous node by one contact pose
 	std::string trajectory_filepath; // path to openrave file
 	OpenRAVE::dReal plan_travel_dist; // dist between motion plan start and goal
 	// planning_time, use chrono library?
+};
+
+struct Motion_plan_cluster {
+	int representative_index;
+	std::vector<Motion_plan> plans;
+};
+
+class Motion_plan_library {
+	// motion plan distance range -> motion plan clusters mapping
+	// map keys represent range [key, key + distance_delta)
+	std::map<OpenRAVE::dReal, std::vector<Motion_plan_cluster>> partitioned_clusters;
+public:
+	// append motion plan to library, updating clusters as necessary
+	void append_plan(const Motion_plan & plan);
+
+	// returns motion plans in order of most likely to fit environment
+	std::vector<Motion_plan> query(const std::vector<Contact_region> & contact_regions) const;
 };
 
 #endif
